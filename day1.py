19290979287bb4156f91562b25f624f33496669f
bryan-barreto <bryan94barreto@gmail.com>
2022-12-01 16:43:16 -0500

    day1

---
+++ day1.py
@@ -0,0 +1,14 @@
+list = open("day1.txt").read().split("\n\n")
+total_list = []
+
+for x in list:
+    current_elf = x.split()
+    elf_carry = 0
+    for y in current_elf:
+        elf_carry += int(y)
+    total_list.append(elf_carry)
+
+total_list.sort(reverse=True)
+
+print("Part 1:\t" + str(total_list[0]))
+print("Part 2:\t" + str(total_list[0] + total_list[1] + total_list[2]))
