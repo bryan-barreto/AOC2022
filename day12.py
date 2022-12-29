c44ea36a5e67f27cc6818302596e86c9b9b72d4d
bryan-barreto <bryan94barreto@gmail.com>
2022-12-28 22:09:14 -0500

    day 12 part 2

--- day12.py
+++ day12.py
@@ -43,20 +43,14 @@ def part1():
     been_there = []
     while 1 > 0:
         counter +=1
-        if counter % 100 == 0:
-            print(len(fringe))
         step = fringe.pop(0)
         
         step_location = step.get_location() 
         for test in (-1,0), (1,0), (0,1), (0, -1):
-            # if step.get_height() >= ord('s'):
-            #     pass
             test = (step_location[0] + test[0], step_location[1] + test[1])
             if test[0] >= len(full_map[0]) or test[0] < 0 or test[1] >= len(full_map) or test[1] < 0:
                 continue
             new_height = ord('z') if full_map[test[1]][test[0]] == 'E' else ord(full_map[test[1]][test[0]])
-            # if new_height >= ord('z'):
-            #     pass
             if have_i(been_there, test):
                 continue               
             if new_height - 1 > step.get_height():
@@ -64,16 +58,57 @@ def part1():
             if test == goal:         
                 to_print = step.get_path().copy()
                 to_print.append(step.get_location())
-                print(to_print)
                 return len(to_print)
             
-            # if step.test_previous(test):
-            #     continue
             path = step.get_path().copy()
             path.append(step.get_location())
             fringe.append(Step(test, new_height, path, goal))
             been_there.append(test)
-        # fringe.sort(key = lambda x : x.height, reverse=True)
-        # fringe.sort(key = lambda x : x.huer)
 
-print(part1())
+
+def part2(x,y):
+    counter = 0
+    goal = find_pos('E')
+    fringe = [Step((x,y), ord('a'), [], goal)]
+    been_there = []
+    while 1 > 0:
+        counter +=1
+        if len(fringe) == 0:
+            return 1000
+        step = fringe.pop(0)
+        
+        step_location = step.get_location() 
+        for test in (-1,0), (1,0), (0,1), (0, -1):
+            test = (step_location[0] + test[0], step_location[1] + test[1])
+            if test[0] >= len(full_map[0]) or test[0] < 0 or test[1] >= len(full_map) or test[1] < 0:
+                continue
+            new_height = ord('z') if full_map[test[1]][test[0]] == 'E' else ord(full_map[test[1]][test[0]])
+            if have_i(been_there, test):
+                continue               
+            if new_height - 1 > step.get_height():
+                continue
+            if test == goal:         
+                to_print = step.get_path().copy()
+                to_print.append(step.get_location())
+                return len(to_print)
+            
+            path = step.get_path().copy()
+            path.append(step.get_location())
+            fringe.append(Step(test, new_height, path, goal))
+            been_there.append(test)
+        
+# print(part1())
+
+shortest = 1000
+ycounter = 0
+for y in full_map:
+    xcounter = 0
+    for x in y:
+        if x != 'a':
+            continue
+        temp = part2(xcounter,ycounter)
+        if temp < shortest:
+            shortest = temp 
+        xcounter += 1
+    ycounter += 1
+print(shortest)
